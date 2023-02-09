a378bae5db82ab481ccc76935892c6f2ff2bac50
Jhollyfer Rodrigues <jhollyfer.fr@gmail.com>
2023-02-09 00:45:54 -0300

    :sparkles: configure home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { ThemeProvider } from 'styled-components';
+import { Home } from './pages/Home';
+import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/theme/default';
 
 export function App() {
 	return (
 		<ThemeProvider theme={defaultTheme}>
-			<h1>Welvome to Web App React Vite Boilerplate</h1>
+			<Home />
+			<GlobalStyle />
 		</ThemeProvider>
 	);
 }
